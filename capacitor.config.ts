ed25fefa25a98765f20d61581cf1f0bffab099f5
Bucci369 <danyal64@hotmail.com>
2025-07-09 19:35:08 +0200

    pwt

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'www.kleindigitalsolutions.de',
+  appName: 'TrackFood',
+  webDir: 'www.trackfood.app'
+};
+
+export default config;
