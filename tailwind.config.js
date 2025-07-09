cdd966d6efc5a6d14377e910c80ce72d60a0bed5
Bucci369 <danyal64@hotmail.com>
2025-07-09 10:50:01 +0200

    ss

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,24 @@ module.exports = {
   // Darkmode komplett deaktivieren
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand1: '#D9ED92', // sehr helles Grün
+        brand2: '#B5E48C',
+        brand3: '#99D98C',
+        brand4: '#76C893',
+        brand5: '#52B69A',
+        brand6: '#34A0A4',
+        brand7: '#168AAD',
+        brand8: '#1A759F',
+        brand9: '#1E6091',
+        brand10: '#184E77', // dunkelstes Blau
+      },
+    },
   },
   plugins: [],
+  safelist: [
+    'from-brand6', 'to-brand4', 'from-brand8', 'to-brand6', 'from-brand3', 'to-brand7', 'from-brand2', 'to-brand5',
+    'bg-brand6', 'bg-brand4', 'bg-brand8', 'bg-brand3', 'bg-brand7', 'bg-brand2', 'bg-brand5'
+  ],
 }
