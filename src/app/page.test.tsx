a6d94292e33bde9bfd9c8d600acd8d0ac8caf232
Bucci369 <danyal64@hotmail.com>
2025-11-07 13:06:38 +0100

    Test 404

---
+++ src/app/page.test.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function TestPage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Test erfolgreich</h1>
+      <p>Wenn du das siehst, funktioniert das Deployment grundsätzlich.</p>
+    </div>
+  )
+}
